17cb64f3f5a6a40e087d8976d5af18c6307e7557
Raniah06 <raniafadil06@gmail.com>
2025-01-16 10:16:22 +0200

    m

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """
-This module contains a type-annotated function `add` that takes two float 
+This module contains a type-annotated function `add` that takes two float
 arguments and returns their sum as a float.
 """
+
 
 def add(a: float, b: float) -> float:
     """Takes two float arguments and returns their sum as a float."""
