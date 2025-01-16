50a00b77047f7d722e37c90cf7eec0f63645dced
Raniah06 <raniafadil06@gmail.com>
2025-01-16 10:51:55 +0200

    m

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """
-This module contains a function `safely_get_value` that retrieves a value 
-from a dictionary using a given key. If the key is not found, it returns 
-the default value.
+This module contains a function `safely_get_value` that retrieves a dictionary value using a given key. If the key is not found, it returns the default value.
 """
 
 from typing import Mapping, Any, Union, TypeVar
