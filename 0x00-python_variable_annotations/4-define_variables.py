0b21bfb08ac8bed0a8e9d285a601f8f0025ff159
Raniah06 <raniafadil06@gmail.com>
2025-01-16 10:21:28 +0200

    m

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module defines and annotates the following variables:
+- a: an integer with a value of 1
+- pi: a float with a value of 3.14
+- i_understand_annotations: a boolean with a value of True
+- school: a string with a value of "ALX"
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "ALX"
